9e680710f52fc6e732b8ff6ff296b14b89c00031
Zhefeng Chen <chzf_zju@163.com>
2021-06-25 03:09:49 +0800

    Site updated: 2021-06-25 03:09:48

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function($) {
                           "达芬奇", "米开朗基罗", "莎士比亚", "弗洛伊德", "特斯拉", "伽利略",
                           "麦克斯韦", "成吉思汗", "贝多芬", "卢梭");
         //a_idx = Math.floor(Math.random()*a.length);
-        var $i = $("<span>").text(a[a_idx]);
+        var $i = $("<span/>").text(a[a_idx]);
         var x = e.pageX,
             y = e.pageY;
         $i.css({
@@ -26,4 +26,3 @@ jQuery(document).ready(function($) {
     });
 });
 
-</span>
